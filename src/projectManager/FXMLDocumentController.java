59f6d162e0a19360248096f82706f865039552ea
Robotteve <60617472+Robotteve@users.noreply.github.com>
2020-03-08 22:44:44 +0100

    Projects táblázatnál a módosítás és az új gombokhoz felugró ablak betöltése, adatokkal.

--- src/projectManager/FXMLDocumentController.java
+++ src/projectManager/FXMLDocumentController.java
@@ -8,13 +8,18 @@ package projectManager;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -87,38 +92,40 @@ public class FXMLDocumentController implements Initializable {
     private Button buttonDeleteTask;
 
     //Gombok metódusai
+    //Project
     @FXML
-    void actionDeleteProject(/*ActionEvent event*/) {
+    void actionNewProject() throws Exception {
+        popUpWindow("New Project", -1);
+    }
+
+    @FXML
+    void actionEditProject() throws Exception {
+        int selectedIndex = tableProjects.getSelectionModel().getSelectedIndex();
+        if (selectedIndex > -1) {
+            popUpWindow("Edit Project", selectedIndex);
+        }
+    }
+
+    @FXML
+    void actionDeleteProject() {
+
+    }
+
+    //Task
+    @FXML
+    void actionNewTask() {
 
     }
 
     @FXML
-    void actionDeleteTask(/*ActionEvent event*/) {
+    void actionEditTask() {
 
     }
 
     @FXML
-    void actionEditProject(/*ActionEvent event*/) {
+    void actionDeleteTask() {
 
     }
-
-    @FXML
-    void actionEditTask(/*ActionEvent event*/) {
-
-    }
-
-    @FXML
-    void actionNewProject(/*ActionEvent event*/) {
-
-    }
-
-    @FXML
-    void actionNewTask(/*ActionEvent event*/) {
-
-    }
-
-    //változó kijelölt projeck indexéhez
-    private int selectedProjectIndex;
 
     //Projekt adatok kiírása
     //kapott int változó: táblázatban kijelölt sor indexe
@@ -131,12 +138,11 @@ public class FXMLDocumentController implements Initializable {
         txProjectPrior.setText(tx.getProjectPrior());
         txProjectExtra.setText(tx.getProjectExtra());
         txProjectDetails.setText(tx.getProjectDetails());
-        //kijelölt projektindex
-        selectedProjectIndex=tx.getProjectID();
+        //kijelölt projekthez tartozó feladatok lekérdezése adatbázisból
         database.loadSelectedProjectTasks(tableTasks.getItems(), tx.getProjectID());
         //projectID lekérése: tx.getProjectID()
     }
-    
+
     //task adatok kiírása
     private void textTaskInfo(int i) {
         if (i == -1) {
@@ -151,6 +157,28 @@ public class FXMLDocumentController implements Initializable {
 
     //DataBase osztály példányosítása
     LoadFromDataBase database = new LoadFromDataBase();
+
+    private void popUpWindow(String windowTitle, int selectedIndex) throws Exception {
+        //Új ablak betöltése ProjectPopUpWindow.fxml alapján
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ProjectPopUpWindow.fxml"));
+        Parent root = loader.load();
+        //Adatok átadása
+        ProjectPopUpWindowController controller = loader.getController();
+        if (selectedIndex > -1) {//módosítás esetén
+            controller.setProject(tableProjects.getItems().get(selectedIndex));
+        } else {//új létrehozásakor
+            controller.setProject(new ProjectInfo(0, "", "", "", "1", ""));
+        }
+        controller.setDatabase(database);
+        //új ablak megnyitása
+        Scene scene = new Scene(root);
+        Stage popUpWindow = new Stage();
+        popUpWindow.setResizable(false);
+        popUpWindow.initModality(Modality.APPLICATION_MODAL);
+        popUpWindow.setScene(scene);
+        popUpWindow.setTitle(windowTitle);
+        popUpWindow.showAndWait();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/projectManager/ProjectPopUpWindowController.java
+++ src/projectManager/ProjectPopUpWindowController.java
@@ -8,6 +8,10 @@ package projectManager;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,12 +20,54 @@ import javafx.fxml.Initializable;
  */
 public class ProjectPopUpWindowController implements Initializable {
 
+    @FXML
+    private ComboBox<String> comboProjectName;
+
+    @FXML
+    private ComboBox<String> comboProjectClient;
+
+    @FXML
+    private ComboBox<String> comboProjectPrior;
+
+    @FXML
+    private TextField textProjectExtra;
+
+    @FXML
+    private TextArea textProjectDetails;
+
+    @FXML
+    void cancel(/*ActionEvent event*/) {
+
+    }
+
+    @FXML
+    void save(/*ActionEvent event*/) {
+
+    }
+
+    private int projectID;
+
+    public void setProject(ProjectInfo prinfo) {
+        this.projectID = prinfo.getProjectID();
+        comboProjectName.setValue(prinfo.getProjectName());
+        comboProjectClient.setValue(prinfo.getProjectClient());
+        comboProjectPrior.setValue(prinfo.getProjectPrior());
+        textProjectExtra.setText(prinfo.getProjectExtra());
+        textProjectDetails.setText(prinfo.getProjectDetails());
+    }
+
+    private LoadFromDataBase database;
+
+    public void setDatabase(LoadFromDataBase database) {
+        this.database = database;
+    }
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        comboProjectPrior.getItems().addAll("1", "2", "3", "4", "5");
+    }
+
 }
